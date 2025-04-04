1e4074820f76d7df3056560863f7a37c2e6d29b7
Brainiks <151562753+Brainiks@users.noreply.github.com>
2025-04-04 17:39:28 +0300

    Add files via upload

---
+++ js/window.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+document.getElementById('button1').addEventListener('click', () => {
+    togglePopup();
+  });
+  
+  document.getElementById('button2').addEventListener('click', () => {
+    // Другая логика для Кнопки 2
+  });
+  
+  document.getElementById('button3').addEventListener('click', () => {
+    // Другая логика для Кнопки 3
+  });
+  
+  function togglePopup() {
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+  }
